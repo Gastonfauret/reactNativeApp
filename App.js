c998d61e893ac5a2e8cd6c73e1b1811794c78964
Gastonfauret <Gastonfauret@gmail.com>
2024-02-16 11:58:17 -0300

    Instalation and first steps DONE

--- App.js
+++ App.js
@@ -3,18 +3,34 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
+
+      <View style={{ height: 100, width: 393, backgroundColor: 'green', flex: 1 }} />
+
+      <View style={styles.subcontainer} />    
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#FBFCFC',    
   },
+
+  subcontainer: {
+    height: 100,
+    width: 393,
+    backgroundColor: 'red',   
+    flex: 1 
+  }
 });
+
+// https://www.youtube.com/watch?v=oD5WpQJo9YY&list=PLUlw6638d2QZFbQfC3sB5mKWJUWr2HEkc

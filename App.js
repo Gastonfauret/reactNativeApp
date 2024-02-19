e84b041f9827a3dfdf57e049be5b8fd6ab0a06b9
Gastonfauret <Gastonfauret@gmail.com>
2024-02-19 12:19:48 -0300

    Counter Done!

--- App.js
+++ App.js
@@ -1,36 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import Button from './components/button/Button';
 
 export default function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  function handleUp() {
+    setCounter(c => c + 1)
+  }
+
+  function handleDown() {
+    setCounter(c => (c > 0 ? c - 1 : 0));
+  }
+
   return (
-
     <View style={styles.container}>
+      
+      <View style={styles.subcontainer}>
 
-      <StatusBar style="auto" />
+        <Button label='-' action={handleDown} />
 
-      <View style={{ height: 100, width: 393, backgroundColor: 'green', flex: 1 }} />
+        <View style={styles.counterContainer}>
+          <Text style={styles.counter} >{counter}</Text>
+        </View>
 
-      <View style={styles.subcontainer} />    
+        <Button label='+' action={handleUp} />
+
+      </View>
 
     </View>
 
-  );
-}
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
-    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#2c3e50',
+    justifyContent: 'center'
+  },
+  subcontainer: {
+    height: 50,
+    width: '100%',
+    paddingHorizontal: 10,
+    flexDirection: 'row'
+  },
+  btn: {
+    width: 50,
+    height: 50,
     justifyContent: 'center',
-    backgroundColor: '#FBFCFC',    
+    alignItems: 'center',
+    backgroundColor: '#ecf0f1'
+  },
+  btnTxt: {
+    fontSize: 24,
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  counterContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 
-  subcontainer: {
-    height: 100,
-    width: 393,
-    backgroundColor: 'red',   
-    flex: 1 
+  counter: {
+    fontSize: 40,
+    color: 'white',
+    fontWeight: 'bold'
   }
 });
 
-// https://www.youtube.com/watch?v=oD5WpQJo9YY&list=PLUlw6638d2QZFbQfC3sB5mKWJUWr2HEkc
+
---
+++ components/button/Button.js
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default function Button(props) {
+    
+    const { label, action } = props;
+
+    return (
+        <View>
+
+            <TouchableOpacity style={styles.btn}
+                onPress={action}>
+                <Text style={styles.btnTxt}>{label}</Text>
+            </TouchableOpacity>
+
+        </View>
+    )
+}
+
+Button.PropTypes = {
+    label: PropTypes.string.isRequired,
+    action: PropTypes.func.isRequired
+}
+
+const styles = StyleSheet.create({    
+    btn: {
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ecf0f1'
+    },
+    btnTxt: {
+        fontSize: 24,
+        color: 'black',
+        fontWeight: 'bold'
+    }
+});
+
+// https://www.youtube.com/watch?v=-Hfdjd0GyZc&list=PLUlw6638d2QZFbQfC3sB5mKWJUWr2HEkc
+
